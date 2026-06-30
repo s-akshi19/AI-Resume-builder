e892996134828d4a11efc2f9dee81f0927166bc6
Sakshi Tripathi <sakshitripathi477@gmail.com>
2026-07-01 02:29:17 +0530

    fix: use env variable for backend URL

--- mern_ai/src/utils/axios.js
+++ mern_ai/src/utils/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: "https://ai-resume-backend.onrender.com",
+    baseURL: import.meta.env.VITE_API_URL,
 })
 export default instance;
